06254802813a7a503ce68f1d07e26e9e7856f570
Pavlo Kilko <pavel.kilko@gmail.com>
2024-05-31 04:14:14 +0300

    feat: add Editor

---
+++ DPAS/Tools/Editor/app.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const imagesButton = document.getElementById('imagesButton');
+  imagesButton.addEventListener('click', () => {
+    window.location.href = 'images.html'; // Redirect to images page
+  });
+});
